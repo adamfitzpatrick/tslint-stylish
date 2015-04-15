a90a45430f804f1da7dc9f36793c221ca54dccc4
adamfitz77hob <adam.fitzpatrick@hobsons.com>
2015-04-15 10:26:18 -0700

    GTS-3 gulp help available

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,9 @@
   var watch = require("gulp-watch");
   var batch = require("gulp-batch");
   var notify = require("gulp-notify");
+  require("gulp-help")(gulp, {
+    description: "Help listing"
+  })
 
   var typescriptOptions = {
     declarationFiles: false,
@@ -26,7 +29,7 @@
       .pipe(tsc(typescriptOptions))
       .js.pipe(gulp.dest("release/"));
   }
-  gulp.task("compile:src", tsCompile);
+  gulp.task("compile:src", "Compile source typescript to javascript.", tsCompile);
 
   function specCompile() {
     return gulp.src("src/*.spec.ts")
@@ -37,7 +40,7 @@
       .pipe(tsc(typescriptOptions))
       .js.pipe(gulp.dest("specs/"));
   }
-  gulp.task("compile:specs", specCompile);
+  gulp.task("compile:specs", "Compile specs typescript to javascript.", specCompile);
 
   function tests() {
     return gulp.src("specs/*.js")
@@ -49,11 +52,11 @@
         "sound": "Hero"
       }));
   }
-  gulp.task("test", ["compile:specs"], tests);
+  gulp.task("test", "Compile specs to javascript and run tests.", ["compile:specs"], tests);
 
-  gulp.task("default", ["compile:src", "compile:specs"], tests);
+  gulp.task("default", "Compile source and specs to javascript and run tests.", ["compile:src", "compile:specs"], tests);
 
-  gulp.task("watch", function () {
+  gulp.task("watch", "Re-compile all source and spec files and run all tests on typescript source change.", function () {
     watch([
       "src/*.ts",
     ], { verbose: true, name: "Source" }, function () {
